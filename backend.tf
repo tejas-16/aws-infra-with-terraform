bc23f34c6874d83620b92861a3f06d62eb8cdddc
tejas-16 <74181918+tejas-16@users.noreply.github.com>
2025-11-07 20:35:30 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,9 +1,3 @@
 terraform {
-  backend "s3" {
-    bucket         = "__BUCKET_NAME__"     # replace after creating bucket
-    key            = "terraform/state/infra.tfstate"
-    region         = "__REGION__"          # replace only once
-    dynamodb_table = "__DDB_TABLE__"       # replace after creating table
-    encrypt        = true
-  }
+  backend "s3" {}
 }
